ecfb57ab4901df2a3297f9863668ea6fedccaabc
Christian Esparza <cesparzajp@gmail.com>
2024-09-29 16:19:53 +0900

    test: react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import { BasicForm } from './Greeting.jsx' 
 import { SampleComponent } from './Sample.jsx'
+import {Profile,DefaultProfile} from './Profile.jsx';
+import {Popeye} from './Popeye.jsx';
+import {Spinach} from './Spinach.jsx';
+import {ErrorPage} from './ErrorPage.jsx';
+import routes from './routes.jsx';
+import Image from './Image.jsx';
+
+const router = createBrowserRouter(routes)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <BasicForm />
-    <SampleComponent/>
+    <Image/>
+    {/* <RouterProvider router={router} /> */}
   </StrictMode>,
 )
